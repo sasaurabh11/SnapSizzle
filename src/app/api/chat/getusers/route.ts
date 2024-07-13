620fb15273798fc3256cce1e38f79c94d8ac1e84
Saurabh <160242066+sasaurabh11@users.noreply.github.com>
2024-07-14 01:54:49 +0530

    error resolve

--- src/app/api/chat/getusers/route.ts
+++ src/app/api/chat/getusers/route.ts
@@ -2,13 +2,12 @@ import { auth } from "@/auth";
 import { ConnectDB } from "@/db/db_config";
 import User from "@/models/user.model";
 import { UserDocumentType } from "@/types";
-import { NextApiRequest, NextApiResponse } from "next";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req : NextRequest) {
     try {
         const authUser = await auth();
-        if(!authUser) return;
+        if(!authUser) return NextResponse.json({ error: "Authentication failed" }, { status: 401 });;
 
         await ConnectDB()
         const users : UserDocumentType[] = await User.find({ _id: { $ne: authUser.user?._id } })
