4821403838794608482a8a7efbbb1880f4b7f8b9
Saurabh <160242066+sasaurabh11@users.noreply.github.com>
2024-07-14 01:58:31 +0530

    error

--- src/app/api/chat/getusers/route.ts
+++ src/app/api/chat/getusers/route.ts
@@ -4,16 +4,19 @@ import User from "@/models/user.model";
 import { UserDocumentType } from "@/types";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req : NextRequest) {
+export async function GET(req: NextRequest) {
     try {
         const authUser = await auth();
-        if(!authUser) return NextResponse.json({ error: "Authentication failed" }, { status: 401 });;
+        if (!authUser) {
+            return NextResponse.json({ error: "Authentication failed" }, { status: 401 });
+        }
 
-        await ConnectDB()
-        const users : UserDocumentType[] = await User.find({ _id: { $ne: authUser.user?._id } })
+        await ConnectDB(); // Ensure database connection
+        const users: UserDocumentType[] = await User.find({ _id: { $ne: authUser.user?._id } });
+
         return NextResponse.json(users);
     } catch (error) {
-        console.log(error);
-        throw new Error("Error fetching users");
+        console.error("Error fetching users:", error);
+        return NextResponse.json({ error: "Error fetching users" }, { status: 500 });
     }
 }
